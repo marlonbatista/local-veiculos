79e652683d2bd3345b2c17ee6ec0ba9142cfe118
Marlon Batisa <marlon.batista18@hotmail.com>
2021-03-16 08:27:41 -0300

    Correção na ClienteController

--- Java/locacao-veiculos-services-master/src/main/java/com/shx/locacao/veiculos/controller/ClienteController.java
+++ Java/locacao-veiculos-services-master/src/main/java/com/shx/locacao/veiculos/controller/ClienteController.java
@@ -17,63 +17,55 @@ import com.shx.locacao.veiculos.model.Cliente;
 import com.shx.locacao.veiculos.repository.ClienteRepository;
 
 @CrossOrigin
-@RestController 
-@RequestMapping(value="/api/v1/cliente",produces = MediaType.APPLICATION_JSON_VALUE )
+@RestController
+@RequestMapping(value = "/api/v1/cliente", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ClienteController {
 
 	@Autowired
 	private ClienteRepository repositorio;
-	
-	
+
 	@GetMapping(path = "/")
-	public List<Cliente> buscarClientes () throws Exception {
+	public List<Cliente> buscarClientes() throws Exception {
 		List<Cliente> clientes = repositorio.findAll();
 		return clientes;
 	}
-		
-	@RequestMapping(path = "/{id}", method= {RequestMethod.GET})
-	public Cliente findById(@PathVariable("id") Integer id) throws Exception{
-		if(id == null) {
+
+	@RequestMapping(path = "/{id}", method = { RequestMethod.GET })
+	public Cliente findById(@PathVariable("id") Integer id) throws Exception {
+		if (id == null) {
 			throw new Exception("Id inválido");
-		} 
+		}
 		return repositorio.findById(id);
 	}
-	
-	@PostMapping(path= "/")
-	public Cliente create(@RequestBody Cliente cliente) throws Exception{
-		if(this.isValid(cliente) == false) {
-			throw new Exception("dados inválidos");	
+
+	@PostMapping(path = "/")
+	public Cliente create(@RequestBody Cliente cliente) throws Exception {
+		if (this.isValid(cliente) == false) {
+			throw new Exception("dados inválidos");
 		}
-		return repositorio.save(cliente);	
+		return repositorio.save(cliente);
 	}
-	
-	
-	@RequestMapping(value="/{id}", method= {RequestMethod.DELETE})
-	 public void delete( @PathVariable("id") Integer id) throws Exception {
-		if(id == null) {
-			throw new Exception("Id inválido");	
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.DELETE })
+	public void delete(@PathVariable("id") Integer id) throws Exception {
+		if (id == null) {
+			throw new Exception("Id inválido");
 		}
 		repositorio.delete(id);
-	 }
-	
+	}
+
 	private boolean isValid(Cliente cliente) {
-		if (cliente.getCpf() == null || 
-				cliente.getCpf().length() > 11 ||
-				cliente.getCpf().length() < 11 ||
-				cliente.getCpf().isBlank() ||
-				cliente.getCpf().isEmpty()
-				) {
-		      return false;
-		    }
-		    if (cliente.getNome() == null || 
-		    		cliente.getNome().isEmpty() || 
-		    		cliente.getNome().isBlank() ||
-		    		cliente.getNome().length() > 100) {
-		      return false;
-		    }
-		    if (cliente.getData_nascimento() == null) {
-		      return false;
-		    }
-		    return true;
+		if (cliente.getCpf() == null || cliente.getCpf().length() > 11 || cliente.getCpf().length() < 11
+				|| cliente.getCpf().isBlank() || cliente.getCpf().isEmpty()) {
+			return false;
+		}
+		if (cliente.getNome() == null || cliente.getNome().isEmpty() || cliente.getNome().isBlank()
+				|| cliente.getNome().length() > 100) {
+			return false;
+		}
+		if (cliente.getData_nascimento() == null) {
+			return false;
+		}
+		return true;
 	}
 }
