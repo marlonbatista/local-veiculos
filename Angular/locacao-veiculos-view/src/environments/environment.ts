fb27fdbe949bfe53ac4b8e08e8c4ded6c20382ad
Marlon Batisa <marlon.batista18@hotmail.com>
2021-03-16 08:26:51 -0300

    Alteração na variavel de conexão como back-end

--- Angular/locacao-veiculos-view/src/environments/environment.ts
+++ Angular/locacao-veiculos-view/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url_api: 'http://localhost:8080/locacao-veiculos-services/api/v1'
+  url_api: 'http://localhost:8090/locacao-veiculos-services/api/v1'
 };
 
 /*
